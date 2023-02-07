5e2445ed954fec3988c4b36ae456e1753b866bf3
Laurence Fairbanks <laurencefairbanks@yahoo.com>
2023-02-06 22:07:21 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// function superbowlWin(record){
+// const ele = record.find(({result}) => result === "W") 
+// if (!!ele) {
+// return ele.year
+// } else {
+//     return undefined
+//     }
+//}
+
+
+
+
+function superbowlWin(record) {
+ const element = record.find(({result}) => result === "W");
+ if(!!element) {
+    return element.year
+    } else {
+    return undefined
+    }
+}
